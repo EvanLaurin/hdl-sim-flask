11f2e4f3ce50df94446d67466a300a96cba1ec9f
EvanLaurin <laurine@uoguelph.ca>
2025-04-25 14:19:58 -0400

    Clean up "Uploads" on start

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes 
+# Ensure uploads folder exists
+os.makedirs('uploads', exist_ok=True)
+
+# Clean out contents of uploads folder
+for filename in os.listdir('uploads'):
+  file_path = os.path.join('uploads', filename)
+  try:
+    if os.path.isfile(file_path):
+      os.remove(file_path)
+  except Exception as e:
+    print(f"Error removing {file_path}: {e}")
+
+from app import routes
